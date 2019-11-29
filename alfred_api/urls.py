fdb2c15709a5864c32b4d5d84f771ea4e18f406b
Przemysław Kuczyński <przemek@kuczynski.dev>
2019-11-29 13:11:59 +0100

    Authentication, owners & permissions

--- alfred_api/urls.py
+++ alfred_api/urls.py
@@ -18,9 +18,9 @@ from django.urls import include, path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
-    
-    path('api/', include('snippets.urls')),
-    path('api/', include('users.urls')),
+
+    path('api-snippets/', include('snippets.urls')),
+    path('api-users/', include('users.urls')),
 
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls'))
