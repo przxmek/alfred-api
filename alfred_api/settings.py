254e85ef2106d3798530217ad6fdbc317a7ee13d
Przemysław Kuczyński <przemek@kuczynski.dev>
2019-11-29 12:06:07 +0100

    Django REST & users endpoints

--- alfred_api/settings.py
+++ alfred_api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'polls.apps.PollsConfig',
 
     'django.contrib.admin',
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Django REST framework config
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
--- alfred_api/urls.py
+++ alfred_api/urls.py
@@ -19,5 +19,8 @@ from django.urls import include, path
 urlpatterns = [
     path('polls/', include('polls.urls')),
 
+    path('rest/', include('users.urls')),
+
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls'))
 ]
