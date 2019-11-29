656a5331649868537a03687d2fd2b1bfe8520d7c
Przemysław Kuczyński <przemek@kuczynski.dev>
2019-11-29 12:54:44 +0100

    Snippets REST api

--- alfred_api/settings.py
+++ alfred_api/settings.py
@@ -31,16 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users.apps.UsersConfig',
-    'polls.apps.PollsConfig',
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
+
+    'users.apps.UsersConfig',
+    'polls.apps.PollsConfig',
+    'snippets.apps.SnippetsConfig',
 ]
 
 MIDDLEWARE = [
--- alfred_api/urls.py
+++ alfred_api/urls.py
@@ -18,8 +18,9 @@ from django.urls import include, path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
-
-    path('rest/', include('users.urls')),
+    
+    path('api/', include('snippets.urls')),
+    path('api/', include('users.urls')),
 
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls'))
